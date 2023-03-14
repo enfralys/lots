30d33c212a9657abcdc75c19c18641c85f039f05
Fernando Aponte <fernandoaponte90@gmail.com>
2023-03-14 13:49:34 -0400

    feat: CRUD eat-status-x-screen

--- src/modules/lots/lots-query.module.ts
+++ src/modules/lots/lots-query.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { EatLotsModule } from './eat-lots/eat-lots.module';
 import { ApplicationModule } from './application/application.module';
 import { TmpLotsEatModule } from './tpm-lots-eat/tmp-lots-eat.module';
+import { EatStatusXScreenModule } from './eat-state-x-screen/eat-status-x-screen.module';
 
 @Module({
-  imports: [EatLotsModule, ApplicationModule, TmpLotsEatModule],
+  imports: [
+    EatLotsModule,
+    ApplicationModule,
+    TmpLotsEatModule,
+    EatStatusXScreenModule
+  ],
 })
-export class LotsQueryModule {}
+export class LotsQueryModule { }
